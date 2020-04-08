f4ab7c25837264d958d19246e4536fc855c65283
Moosa Hassan <moosa7021@gmail.com>
2020-04-08 17:54:55 +1000

    created a new circle java class with base functions to get started

---
+++ Circle.java
@@ -0,0 +1,29 @@
+// 2.6 Circle Class
+
+public class Circle{
+
+
+
+
+
+    public double calculateArea(){
+        // method use area = pi * radius squared
+        return 0;
+    }
+
+    public double originDistance(){
+        // distance = (distance from origin of centre) - (radius of circle, which can be negative)
+
+        return 0;
+    }
+
+    // example input data for a circle: C 5.1 4.0 3.2
+    public String toString(){
+        String line = "CIRC=";
+
+        // still needs to be coded properly
+        line = "[point0 r]: " + calculateArea();
+
+        return line;
+    }
+}
