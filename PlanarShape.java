72a8c323197ca5bcf46fdd310f4e9531d44c85fc
Moosa Hassan <moosa7021@gmail.com>
2020-04-09 16:52:44 +1000

    commented out for later use

--- PlanarShape.java
+++ PlanarShape.java
@@ -7,22 +7,27 @@ public abstract class PlanarShape{
     abstract void draw();
 
 
-
-    abstract String toString(){
+    /*
+    public abstract String toString(){
         // method
+        String line = "";
+
+        return line;
+        
     }
 
     // this will be the basis for ordering
-    abstract double calculateArea(){
+    public abstract double calculateArea(){
         // method
         return 0;
     }
 
     // this will also be the basis for ordering (same thing as assignment1)
-    abstract double originDistance(){
+    public abstract double originDistance(){
         // method
         return 0;
     }
+    */
 
     // standard Comparable<T> interface are also meant to be implemented as you are comparing PlanarShape objects
 }
