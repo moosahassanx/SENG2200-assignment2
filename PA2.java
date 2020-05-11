49a01296f691cabd94365f6c5650b445bfd3bf4e
Moosa Hassan <moosa7021@gmail.com>
2020-05-11 16:07:37 +1000

    changed commenting

--- PA2.java
+++ PA2.java
@@ -1,8 +1,8 @@
 // TITLE: 					Assignment2
 // COURSE: 					SENG2200
 // AUTHOR: 					Moosa Hassan
-// STUDENT NUMBER: 			3331532 
-// DATE: 					15/05/2020 
+// STUDENT NUMBER: 			3331532
+// DATE: 					15/05/2020
 // DESCRIPTION: 			main file used for text scanning and final display
 
 // importing java libraries (file scanner and string output)
@@ -11,69 +11,60 @@ import java.util.Scanner;
 
 class PA2{
 	public static void main(final String[] args) throws IOException {
-		// create new linked list which will be unsorted
-		final MyPolygons unsortedList = new MyPolygons();
-
-		// create file scanner
-		final Scanner file = new Scanner(new File(args[0]));
-		// declare and instantiate string to store scanner inputs
+		final MyPolygons unsortedList = new MyPolygons();			// unsorted linked list
+		final Scanner file = new Scanner(new File(args[0]));		// EXECUTE: java PA2 inputfile.txt
 		String newText = "";
 
 		try {
-			// loop as long as scanner can read another character
+			// scanner keeps looking for next character
 			while (file.hasNext()) {
-				// declare string and assign input
-				newText = file.next();
+				newText = file.next();							// declare string and assign input
 
-				if (newText.equals("P")) { // scanner reads "P"
-					// declare int after P as number of points
-					final int numOfPoints = file.nextInt();
+				// case1: scanner detects polygon
+				if (newText.equals("P")) {
+					final int numOfPoints = file.nextInt();						// next int is assumed to be number of points
+					final Polygon polygonObject = new Polygon(numOfPoints);		// new polygon object with number of points
+					final Point[] pointArray = new Point[numOfPoints];			//  declare point array and set number of elements as number of points
 
-					// declare new polygon object and set paranthesis with number of points
-					final Polygon polygonObject = new Polygon(numOfPoints);
-
-					// declare point array and set number of elements as number of points
-					final Point[] pointArray = new Point[numOfPoints];
-
-					// declare int variables and instantiate
-					int i = 1, j = 0;
+					int i = 1, j = 0;								// declare int variables and instantiate
 					while (i <= numOfPoints) {
-						// declare x and y doubles and instantiate based on file input
+						// x and y alternates
 						final double x = file.nextDouble();
 						final double y = file.nextDouble();
 
-						// declare point array and create new point
-						pointArray[j] = new Point(x, y);
-
-						// insert point into polygon object
-						polygonObject.insertPoint(x, y);
+						pointArray[j] = new Point(x, y);			// create new point element
+						
+						polygonObject.insertPoint(x, y);			// inserting those points
+						
 						// iterate after each loop
 						j++;
 						i++;
 					}
 
-					// append polygon object into linked list
-					unsortedList.append(polygonObject);
+					unsortedList.append(polygonObject);				// append polygon object into linked list
 				}
-				else if(newText.equals("C")){
 
+				// case2: scanner detects circle
+				else if(newText.equals("C")){
 					double x = file.nextDouble();
 					double y = file.nextDouble();
-					double r = file.nextDouble();
+					double r = file.nextDouble();			// radius
 
+					// create new circle using parameters
 					Circle circleObject  = new Circle(r);
-
 					circleObject.insertPoint(x, y);
 
 					// for testing
 					System.out.println(circleObject.toString());
 				}
-				else if(newText.equals("S")){
-					
-					SemiCircle semiCircleObject = new SemiCircle();
 
+				// case3: scanner detects semicircle
+				else if(newText.equals("S")){
+					// new semicircle
+					SemiCircle semiCircleObject = new SemiCircle();
 					final Point[] pointArray = new Point[2];
 
+					// inserting points
 					for(int i = 0; i < 2; i++){
 						double x = file.nextDouble();
 						double y = file.nextDouble();
@@ -81,24 +72,22 @@ class PA2{
 						semiCircleObject.insertPoint(x, y);
 					}
 
+					// for testing
 					System.out.println(semiCircleObject.toString());
 				}
 			}
 		} catch (final Exception e) {
-			System.out.println("Error with reading files");
+			System.out.println("FILE ERROR: " + e);
 		}
 
-		// close file after reading all characters
-		file.close();
+		file.close();		// close file (good practice)
 
 		// print unsorted list
 		System.out.println("Unsorted List:");
 		System.out.println(unsortedList.toString());
 
-		// create new MyPolgyons and set it as unsortedList but after insertSorted() method
-		final MyPolygons sortedList = unsortedList.insertSort();
-		
 		// print sorted list
+		final MyPolygons sortedList = unsortedList.insertSort();
 		System.out.println(sortedList.toString());
 	}
 }
@@ -106,17 +95,16 @@ class PA2{
 /* CODE NEEDS TO READ
 P 5 4.0 0 4 8.1 7.2 8 7 3 9 0
 C 5.1 4.0 3.2
-S 4 3 8.0 6.0
+S 4 3 5.0 4.0
 */
 
-
-/*
+/* OUTPUT
 Unsorted list
+SEMI=[(4.00 , 3.00)(5.00 , 4.00)]: 39.27
+CIRC=[(5.10 , 4.00)) 3.20]: 32.17
 POLY=[(4.00 , 0.00)(4.00 , 8.10)(7.20 , 8.00)(7.00 , 3.00)(9.00 , 0.00)]: 27.66
-CIRC=[(5.10 , 4.00)) 3.20]: 5.62
-SEMI=[(4.00 , 3.00)(5.00 , 4.00)]: 3.51
 Sorted list
-SEMI=[(4.00 , 3.00)(5.00 , 4.00)]: 3.51
-CIRC=[(5.10 , 4.00)) 3.20]: 5.62
 POLY=[(4.00 , 0.00)(4.00 , 8.10)(7.20 , 8.00)(7.00 , 3.00)(9.00 , 0.00)]: 27.66
+CIRC=[(5.10 , 4.00)) 3.20]: 32.17
+SEMI=[(4.00 , 3.00)(5.00 , 4.00)]: 39.27
 */
